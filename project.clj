eb2c2a8672128a2eadbcf60d4ee754403e86e7f6
Ian Barrick <ian@servisuite.com>
2019-02-05 12:37:05 -0500

    updated hikari dependency

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject hikari-cp "1.8.0"
+(defproject ibarrick/hikari-cp "1.9.0"
   :description "A Clojure wrapper to HikariCP JDBC connection pool"
-  :url "https://github.com/tomekw/hikari-cp"
+  :url "https://github.com/ibarrick/hikari-cp"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
-        :url  "https://github.com/tomekw/hikari-cp"}
+        :url  "https://github.com/ibarrick/hikari-cp"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.tobereplaced/lettercase "1.0.0"]
-                 [com.zaxxer/HikariCP "2.7.1"]
+                 [com.zaxxer/HikariCP "3.3.0"]
                  [prismatic/schema "1.1.6"]]
   :deploy-repositories [["clojars" {:sign-releases false :url "https://clojars.org/repo"}]]
   :profiles {:dev {
